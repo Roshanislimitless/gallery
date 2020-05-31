cc286340b3a1ef4fed5bebcd78b1968522f659fb
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-05-31 23:49:22 +0200

    gallery totally my way....level 1

--- app.js
+++ app.js
@@ -1,45 +1,64 @@
 const URL =
   "https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json";
 
-/*
-fetch('url')
-.then(Resolve)
-.then(Reject) OR .catch(Reject)
 
-OR directly 
+  fetch(URL)
+  .then((response) => response.json())
+  .then((myJson) => finalGallery(myJson))
 
-fetch('url').then(ResolveFuntion, RejectFuntion)
 
-*/
-fetch(URL)
-  .then((response) => response.json().then((photos) => onPhotosReady(photos)))
-  .catch((err) => console.error(err));
+  const finalGallery = (something) => {
+    forRowOnly();
+    forImageAndColumnDelay(something)
+} 
 
-const app = document.querySelector("#app");
-
-const onPhotosReady = (photos) => {
-  // HINT
-  // if i===4-1 ? create a row  else create a column
-  // i===8-1 ? create a row else create a column
-  // i===12-1 ? create a row else create a column
-  for (let i = 0; i < photos.length; i++) {
-    const element = photos[i];
-    console.log("What is this", element);
-
-    const { url } = photos[i];
-    setTimeout(() => addEachImage(url), 300 * i);
+  const forImageAndColumnDelay = (something) => {
+    
+    for (let i = 0; i < something.length; i++) {
+      const element = something[i];
+      const index = i;
+      //console.log(element);
+      
+      setTimeout( () => forImageAndColumn(element), 400 * index)
+     
+    }
   }
-};
 
-const addEachImage = (url) => {
-  const divRow = document.createElement("div");
-  divRow.className = "row";
-  const divColumn = document.createElement("div");
-  divColumn.className = "column";
-  const image = document.createElement("img");
-  image.src = url;
+  const forRowOnly = ()=>{
+    //console.log(something);
+    const containerDiv = document.querySelector('#app');
+    const divRow = document.createElement('div');
+    divRow.className = 'row';
+    containerDiv.appendChild(divRow)
 
-  divColumn.appendChild(image);
-  divRow.appendChild(divColumn);
-  app.appendChild(divRow);
-};
+  }
+  
+  const forImageAndColumn = (element) =>{
+     const {
+       url,
+       author,
+       pubdate,
+       upvotes,
+       views
+     } = element;
+     //console.log(pubdate);
+     const divRow = document.querySelector('.row');
+
+     const column = document.createElement('div');
+     column.className = 'column';
+
+     const image = document.createElement('img');
+     image.src = url;
+
+     divRow.appendChild(column);
+     column.appendChild(image);
+
+
+  }
+
+
+
+  
+
+
+
